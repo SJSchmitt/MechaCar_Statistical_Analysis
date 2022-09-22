e3c8ad1ae7dd87327d32e331cd8c7f7055a59873
Sam Schmitt <sjschmitt13@gmail.com>
2022-09-22 17:39:56 -0500

    Calculate summary statistics for suspension coils by lot and overall

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,12 @@
 library(dplyr)
 
-mechacar <- read.csv('MechaCar_mpg.csv') #read MechaCar_mpg.csv and save as Dataframe
-
 # Deliverable 1
+mechacar <- read.csv('MechaCar_mpg.csv') #read MechaCar_mpg.csv and save as Dataframe
 mpg_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mechacar)
 summary(mpg_lm)
+
+# Deliverable 2
+susp_coil <- read.csv('Suspension_Coil.csv') # read Suspension_Coil.csv and save as Dataframe
+total_summary <- susp_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) # get summary stats for all suspension coils
+lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) # get summary stats for suspension coils by lot
+
